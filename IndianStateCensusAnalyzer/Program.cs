bcb56f94c5d413e08aa82570c6c89334a032a092
Shivani21-hude <shivani21hude@gmail.com>
2023-04-16 23:26:27 +0530

    [Shivani]-Indian State Sesus information using design principles

---
+++ IndianStateCensusAnalyzer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IndianStateCensusAnalyser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Problem");
+        }
+    }
+}
